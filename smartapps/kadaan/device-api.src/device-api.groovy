0ed4078b0dac2697aadfa7295fd0624e820bd359
Joel Baranick <jbaranick@gmail.com>
2018-12-06 01:46:32 -0800

    Fixed device selection

--- smartapps/kadaan/device-api.src/device-api.groovy
+++ smartapps/kadaan/device-api.src/device-api.groovy
@@ -33,6 +33,7 @@ definition(
 
 preferences {
   page(name: "pageSettings")
+  page(name: "pageSelectDevices")
 }
 
 def installed() {
@@ -89,7 +90,7 @@ def pageSettings() {
 
 private pageSelectDevices() {
   dynamicPage(name: "pageSelectDevices", title: "") {
-    section() {
+	section() {
       paragraph "Select the devices you want API to have access to."
     }
     section ('Select devices by type') {
@@ -97,7 +98,6 @@ private pageSelectDevices() {
       input "dev:actuator", "capability.actuator", multiple: true, title: "Which actuators", required: false
       input "dev:sensor", "capability.sensor", multiple: true, title: "Which sensors", required: false
     }
-
     section ('Select devices by capability') {
       paragraph "If you cannot find a device by type, you may try looking for it by category below"
       def d
@@ -107,4 +107,87 @@ private pageSelectDevices() {
       }
     }
   }
+}
+
+private static Map capabilities() {
+    //n = name
+    //d = friendly devices name
+    //a = default attribute
+    //c = accepted commands
+    //m = momentary
+    //s = number of subdevices
+    //i = subdevice index in event data
+	return [
+		accelerationSensor			: [ n: "Acceleration Sensor",			d: "acceleration sensors",			a: "acceleration",																																																							],
+		actuator					: [ n: "Actuator", 						d: "actuators",																																																																	],
+		alarm						: [ n: "Alarm",							d: "alarms and sirens",				a: "alarm",								c: ["off", "strobe", "siren", "both"],																																								],
+		audioNotification			: [ n: "Audio Notification",			d: "audio notification devices",											c: ["playText", "playTextAndResume", "playTextAndRestore", "playTrack", "playTrackAndResume", "playTrackAndRestore"],				 																],
+		battery						: [ n: "Battery",						d: "battery powered devices",		a: "battery",																																																								],
+		beacon						: [ n: "Beacon",						d: "beacons",						a: "presence",																																																								],
+		bulb						: [ n: "Bulb",							d: "bulbs",							a: "switch",							c: ["off", "on"],																																													],
+		button						: [ n: "Button",						d: "buttons",						a: "button",				m: true,	s: "numberOfButtons,numButtons", i: "buttonNumber",																																					],
+		carbonDioxideMeasurement	: [ n: "Carbon Dioxide Measurement",	d: "carbon dioxide sensors",		a: "carbonDioxide",																																																							],
+		carbonMonoxideDetector		: [ n: "Carbon Monoxide Detector",		d: "carbon monoxide detectors",		a: "carbonMonoxide",																																																						],
+		colorControl				: [ n: "Color Control",					d: "adjustable color lights",		a: "color",								c: ["setColor", "setHue", "setSaturation"],																																							],
+		colorTemperature			: [ n: "Color Temperature",				d: "adjustable white lights",		a: "colorTemperature",					c: ["setColorTemperature"],																																											],
+		configuration				: [ n: "Configuration",					d: "configurable devices",													c: ["configure"],																																													],
+		consumable					: [ n: "Consumable",					d: "consumables",					a: "consumableStatus",					c: ["setConsumableStatus"],																																											],
+		contactSensor				: [ n: "Contact Sensor",				d: "contact sensors",				a: "contact",																																																								],
+		doorControl					: [ n: "Door Control",					d: "automatic doors",				a: "door",								c: ["close", "open"],																																												],
+		energyMeter					: [ n: "Energy Meter",					d: "energy meters",					a: "energy",																																																								],
+		estimatedTimeOfArrival		: [ n: "Estimated Time of Arrival", 	d: "moving devices (ETA)",			a: "eta",																																																									],
+		garageDoorControl			: [ n: "Garage Door Control",			d: "automatic garage doors",		a: "door",								c: ["close", "open"],																																												],
+		holdableButton				: [ n: "Holdable Button",				d: "holdable buttons",				a: "button",				m: true,	s: "numberOfButtons,numButtons", i: "buttonNumber",																																					],
+		illuminanceMeasurement		: [ n: "Illuminance Measurement",		d: "illuminance sensors",			a: "illuminance",																																																							],
+		imageCapture				: [ n: "Image Capture",					d: "cameras, imaging devices",		a: "image",								c: ["take"],																																														],
+		indicator					: [ n: "Indicator",						d: "indicator devices",				a: "indicatorStatus",					c: ["indicatorNever", "indicatorWhenOn", "indicatorWhenOff"],																																		],
+		infraredLevel				: [ n: "Infrared Level",				d: "adjustable infrared lights",	a: "infraredLevel",						c: ["setInfraredLevel"],																																											],
+		light						: [ n: "Light",							d: "lights",						a: "switch",							c: ["off", "on"],																		 																											],
+		lock						: [ n: "Lock",							d: "electronic locks",				a: "lock",								c: ["lock", "unlock"],	s:"numberOfCodes,numCodes", i: "usedCode", 																									 								],
+		lockOnly					: [ n: "Lock Only",						d: "electronic locks (lock only)",	a: "lock",								c: ["lock"],																																														],
+		mediaController				: [ n: "Media Controller",				d: "media controllers",				a: "currentActivity",					c: ["startActivity", "getAllActivities", "getCurrentActivity"],																																		],
+		momentary					: [ n: "Momentary",						d: "momentary switches",													c: ["push"],																																														],
+		motionSensor				: [ n: "Motion Sensor",					d: "motion sensors",				a: "motion",																																																								],
+        musicPlayer					: [ n: "Music Player",					d: "music players",					a: "status",							c: ["mute", "nextTrack", "pause", "play", "playTrack", "previousTrack", "restoreTrack", "resumeTrack", "setLevel", "setTrack", "stop", "unmute"],													],
+		notification				: [ n: "Notification",					d: "notification devices",													c: ["deviceNotification"],																																											],
+		outlet						: [ n: "Outlet",						d: "lights",						a: "switch",							c: ["off", "on"],																																										 			],
+		pHMeasurement				: [ n: "pH Measurement",				d: "pH sensors",					a: "pH",																																																									],
+        polling						: [ n: "Polling",						d: "pollable devices",														c: ["poll"],																																														],
+		powerMeter					: [ n: "Power Meter",					d: "power meters",					a: "power",																																																									],
+		powerSource					: [ n: "Power Source",					d: "multisource powered devices",	a: "powerSource",																																																							],
+		presenceSensor				: [ n: "Presence Sensor",				d: "presence sensors",				a: "presence",																																																								],
+		refresh						: [ n: "Refresh",						d: "refreshable devices",													c: ["refresh"],																																														],
+		relativeHumidityMeasurement	: [ n: "Relative Humidity Measurement",	d: "humidity sensors",				a: "humidity",																																																								],
+		relaySwitch					: [ n: "Relay Switch",					d: "relay switches",				a: "switch",							c: ["off", "on"],																																													],
+		sensor						: [ n: "Sensor",						d: "sensors",						a: "sensor",																																																								],
+		shockSensor					: [ n: "Shock Sensor",					d: "shock sensors",					a: "shock",																																																									],
+		signalStrength				: [ n: "Signal Strength",				d: "wireless devices",				a: "rssi",																																																									],
+		sleepSensor					: [ n: "Sleep Sensor",					d: "sleep sensors",					a: "sleeping",																																																								],
+		smokeDetector				: [ n: "Smoke Detector",				d: "smoke detectors",				a: "smoke",																																																									],
+		soundPressureLevel			: [ n: "Sound Pressure Level",			d: "sound pressure sensors",		a: "soundPressureLevel",																																																					],
+		soundSensor					: [ n: "Sound Sensor",					d: "sound sensors",					a: "sound",																																																									],
+		speechRecognition			: [ n: "Speech Recognition",			d: "speech recognition devices",	a: "phraseSpoken",			m: true,																																																		],
+		speechSynthesis				: [ n: "Speech Synthesis",				d: "speech synthesizers",													c: ["speak"],																																														],
+		stepSensor					: [ n: "Step Sensor",					d: "step counters",					a: "steps",																																																									],
+		switch						: [ n: "Switch",						d: "switches",						a: "switch",							c: ["off", "on"],																																										 			],
+		switchLevel					: [ n: "Switch Level",					d: "dimmers and dimmable lights",	a: "level",								c: ["setLevel"],																																													],
+		tamperAlert					: [ n: "Tamper Alert",					d: "tamper sensors",				a: "tamper",																																																								],
+		temperatureMeasurement		: [ n: "Temperature Measurement",		d: "temperature sensors",			a: "temperature",																																																							],
+		thermostat					: [ n: "Thermostat",					d: "thermostats",					a: "thermostatMode",					c: ["auto", "cool", "emergencyHeat", "fanAuto", "fanCirculate", "fanOn", "heat", "off", "setCoolingSetpoint", "setHeatingSetpoint", "setSchedule", "setThermostatFanMode", "setThermostatMode"],	],
+		thermostatCoolingSetpoint	: [ n: "Thermostat Cooling Setpoint",	d: "thermostats (cooling)",			a: "coolingSetpoint",					c: ["setCoolingSetpoint"],																																											],
+		thermostatFanMode			: [ n: "Thermostat Fan Mode",			d: "fans",							a: "thermostatFanMode",					c: ["fanAuto", "fanCirculate", "fanOn", "setThermostatFanMode"],																																	],
+		thermostatHeatingSetpoint	: [ n: "Thermostat Heating Setpoint",	d: "thermostats (heating)",			a: "heatingSetpoint",					c: ["setHeatingSetpoint"],																																											],
+		thermostatMode				: [ n: "Thermostat Mode",													a: "thermostatMode",					c: ["auto", "cool", "emergencyHeat", "heat", "off", "setThermostatMode"],																															],
+		thermostatOperatingState	: [ n: "Thermostat Operating State",										a: "thermostatOperatingState",																																																				],
+		thermostatSetpoint			: [ n: "Thermostat Setpoint",												a: "thermostatSetpoint",																																																					],
+		threeAxis					: [ n: "Three Axis Sensor",				d: "three axis sensors",			a: "orientation",																																																							],
+		timedSession				: [ n: "Timed Session",					d: "timers",						a: "sessionStatus",						c: ["cancel", "pause", "setTimeRemaining", "start", "stop", ],																																		],
+		tone						: [ n: "Tone",							d: "tone generators",														c: ["beep"],																																														],
+		touchSensor					: [ n: "Touch Sensor",					d: "touch sensors",					a: "touch",																																																									],
+		ultravioletIndex			: [ n: "Ultraviolet Index",				d: "ultraviolet sensors",			a: "ultravioletIndex",																																																						],
+		valve						: [ n: "Valve",							d: "valves",						a: "valve",								c: ["close", "open"],																																												],
+		voltageMeasurement			: [ n: "Voltage Measurement",			d: "voltmeters",					a: "voltage",																																																								],
+		waterSensor					: [ n: "Water Sensor",					d: "water and leak sensors",		a: "water",																																																									],
+		windowShade					: [ n: "Window Shade",					d: "automatic window shades",		a: "windowShade",						c: ["close", "open", "presetPosition"],																																								],
+	]
 }
